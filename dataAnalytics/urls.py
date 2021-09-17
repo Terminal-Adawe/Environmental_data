d9310d1abf5db14736b4dd7ecd76093fd0a87e91
bede abbe <bede.abbe91@gmail.com>
2021-09-17 02:11:12 +0000

    url removal

--- dataAnalytics/urls.py
+++ dataAnalytics/urls.py
@@ -32,10 +32,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 ]
 
-urlpatterns += [
-   re_path('react/',render_react)  
-]
-
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
